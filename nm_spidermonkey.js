dfe839e52ceab2438b91168eab57ca2099020892
guest271314 <guest271314@gmail.com>
2024-06-23 06:53:38 -0700

    Persistence

--- nm_spidermonkey.js
+++ nm_spidermonkey.js
@@ -1,67 +1,71 @@
-#!/usr/bin/env -S JS_STDERR=err.txt /home/user/.jsvu/spidermonkey/spidermonkey
-// /home/user/bin/js
-// SpiderMonkey Shell Native Messaging host (W.I.P.)
+#!/usr/bin/env -S JS_STDERR=err.txt /home/user/.jsvu/engines/spidermonkey/spidermonkey
+// /home/user/bin/jsshell-linux-x86_64/js
+// SpiderMonkey Shell Native Messaging host
 // guest271314 7-7-2023, 6-16-2024
 
 function encodeMessage(str) {
-    return new Uint8Array([...str].map((s) => s.codePointAt()));
-  }
-  // Call readline() twice to catch `\r\n\r\n"`
-  let done = false;
-  
-  function getMessage() {
-    const stdin = readline();
-    const data = encodeMessage(stdin); // new Uint8Array([...stdin].map((s) => s.codePointAt()));
-    const view = new DataView(data.buffer);
-    const length = view.getUint32(0, true);
-    const message = data.subarray(4);
-    if (done) {
-      return void 0;
-    }
-    if (!done) {
-      // https://stackoverflow.com/a/52434176
-      // const previous = redirect("length.txt");
-      // putstr(`${length}`);
-      // redirect(previous); // restore the redirection to stdout
-      // os.file.writeTypedArrayToFile("input.txt", message);
-      done = true;
-      return message;
+  return new Uint8Array([...str].map((s) => s.codePointAt()));
+}
+// Call readline() N times to catch `\r\n\r\n"` from 2d port.postMessage()
+let done = false;
+
+let reads = 0;
+
+function getMessage() {
+  let stdin;
+  while (true) {
+    stdin = readline();
+    if (stdin !== null) {
+      break;
     }
   }
-  
-  function sendMessage(message) {
-    os.file.writeTypedArrayToFile(
-      "/proc/self/fd/1",
-      new Uint32Array([message.length]),
-    );
-    os.file.writeTypedArrayToFile("/proc/self/fd/1", message);
+  // TODO: Handle the *string* input "{}"
+  let data = `${stdin}`.replace(/[\r\n]+|\\x([0-9A-Fa-f]{2,4})/gu, "")
+    .replace(/[^A-Za-z0-9\s\[,\]\{\}:_"]+/igu, "")
+    .replace(/^"rnrn/gu, "")
+    .replace(/^[#\r\n\}_]+(?=\[)/gu, "")
+    .replace(/^"+(?=["\{]+)|^"(?!"$)/gu, "") 
+    .replace(/^\[(?=\[(?!.*\]{2}$))/gu, "")
+    .replace(/^\{(?!\}|.+\}$)/gu, "")
+    .replace(/^[0-9A-Z]+(?=[\[\{"])/igu, "") 
+    .replace(/^[\]\}](?=\[)/i, "")
+    .trimStart().trim();
+  // let previous = redirect("length.txt");
+  // putstr(data.length);
+  // redirect(previous); // restore the redirection to stdout
+  // os.file.writeTypedArrayToFile("input.txt", encodeMessage(data));
+  return encodeMessage(data);
+}
+
+function sendMessage(message) {
+  os.file.writeTypedArrayToFile(
+    "/proc/self/fd/1",
+    new Uint32Array([message.length]),
+  );
+  os.file.writeTypedArrayToFile("/proc/self/fd/1", message);
+}
+
+function main() {
+  // Send help() to client
+  // const previous = redirect("help.txt");
+  // putstr(help());
+  // redirect(previous); // restore the redirection to stdout
+  // const h = read("help.txt", "binary");
+  // sendMessage(encodeMessage(JSON.stringify([...h])));
+  while (true) {
+    const message = getMessage();
+    sendMessage(message);
   }
-  
-  function main() {
-    // Send help() to client
-    // const previous = redirect("help.txt");
-    // putstr(help());
-    // redirect(previous); // restore the redirection to stdout
-    // const h = read("help.txt", "binary");
-    // sendMessage(encodeMessage(JSON.stringify([...h])));
-    // TODO: Make this persistent, comparable to JavaScript runtimes
-    while (true) {
-      const message = getMessage();
-      if (message) {
-        sendMessage(message);
-      } else {
-        break;
-      }
-    }
-  }
-  
-  try {
-    main();
-  } catch (e) {
-    os.file.writeTypedArrayToFile(
-      "caught.txt",
-      encodeMessage(JSON.stringify(e.message)),
-    );
-    quit();
-  }
+}
+
+try {
+  main();
+} catch (e) {
+  os.file.writeTypedArrayToFile(
+    "caught.txt",
+    encodeMessage(JSON.stringify(e.message)),
+  );
+  quit();
+}
+
   

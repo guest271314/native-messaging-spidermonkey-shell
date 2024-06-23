5d47c7b2ef84f2c03026c46e81fba33f524d8833
guest271314 <guest271314@gmail.com>
2024-06-23 06:48:12 -0700

    Persistence

--- background.js
+++ background.js
@@ -1,28 +1,19 @@
 globalThis.name = chrome.runtime.getManifest().short_name;
 
-async function sendNativeMessage(message) {
-  // let jshelp;
-  return new Promise((resolve, reject) => {
-    globalThis.port = chrome.runtime.connectNative(globalThis.name);
-    port.onMessage.addListener((message) => {
-      // if (!jshelp) {
-      //  jshelp = new TextDecoder().decode(new Uint8Array(message));
-      // } else {
-        // console.log(`\x1B[38;2;0;0;255;1m${jshelp}`);
-        resolve(message);
-        port.disconnect();
-      // }
-    });
-    port.onDisconnect.addListener(() => {
-      reject(chrome.runtime.lastError);
-    });
-    port.postMessage(message);
-    // Hack to get the message echoed back from SpiderMonkey shell
-    // TODO: Make this persistent, comparable to JavaScript runtimes
-    port.postMessage("\r\n\r\n");
-  });
-}
+globalThis.port = chrome.runtime.connectNative(globalThis.name);
 
-globalThis.sendNativeMessage = sendNativeMessage;
+port.onMessage.addListener((message) => {
+  console.log(message);
+});
 
-sendNativeMessage(new Array(209715)).then(console.log).catch(console.error);
+port.onDisconnect.addListener(() => {
+  console.log(chrome.runtime.lastError);
+});
+
+globalThis.postNativeMessage = (message) => {
+  port.postMessage(message);
+  // SpiderMonkey shell won't close STDIN without this trailing newline
+  port.postMessage("\r\n\r\n");
+};
+
+postNativeMessage(Array(209715));
